980b452bd596afa7d46b428b5f9bdbff856cbdbf
YinaePark <dlsoso1@unist.ac.kr>
2024-12-07 01:46:10 +0900

    chore: comment

--- domain/item/crops/Corn.java
+++ domain/item/crops/Corn.java
@@ -4,6 +4,6 @@ import domain.item.HarvestItem;
 
 public class Corn extends HarvestItem {
     public Corn() {
-        super("Corn", 2, 5); // 가격 2, 성장 시간 3
+        super("Corn", 2, 5);
     }
 }
--- domain/item/crops/Lettuce.java
+++ domain/item/crops/Lettuce.java
@@ -4,6 +4,6 @@ import domain.item.HarvestItem;
 
 public class Lettuce extends HarvestItem {
     public Lettuce() {
-        super("Lettuce", 2, 3); // 가격 2, 성장 시간 3
+        super("Lettuce", 2, 3);
     }
 }
